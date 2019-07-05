a0c93a70abfd5c9f13679bdcf8bd5ac880587497
Valentin Grouès <valentin.groues@uni.lu>
2019-07-05 10:09:09 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
     from distutils.core import setup
 
 requirements = [
-    'Django==2.1.9',
+    'Django==2.1.10',
     'django-auth-ldap==1.7.0',
     'django-celery-beat==1.4.0',
     'django-celery-results==1.0.4',
