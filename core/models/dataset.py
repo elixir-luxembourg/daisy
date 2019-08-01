aa665c8692d24adbb90b646a6b132237e605a8ef
Pinar Alper <pinar.alper@uni.lu>
2019-08-01 17:45:27 +0200

    harmonised validation style on dataset and project pages also informative messages further fixes to #6

--- core/models/dataset.py
+++ core/models/dataset.py
@@ -77,9 +77,9 @@ class Dataset(CoreTrackedModel):
     def collect_contracts(self):
         result = set()
         for share in self.shares.all():
-            result.add(share.contract)
+            result.add((share.contract, share))
         for ddec in self.data_declarations.all():
-            result.add(ddec.contract)
+            result.add((ddec.contract,ddec))
         return result
 
     def __str__(self):
