801d2ea0b4f9afa01383e64c89f33ace9c5b154a
Valentin Grouès <valentin.groues@uni.lu>
2019-08-05 16:32:35 +0200

    fix migrations script

--- core/migrations/0009_storage_duration.py
+++ core/migrations/0009_storage_duration.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0007_auto_20190731_1014'),
+        ('core', '0008_user_source_field'),
     ]
 
     operations = [
