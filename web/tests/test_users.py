d3f650cdf5b863f473f9a528abcc44abdef5a58c
Yohan Jarosz <yohanjarosz@yahoo.fr>
2019-08-08 09:50:41 +0200

    change filename

---
+++ web/tests/test_users.py
@@ -0,0 +1,46 @@
+import pytest
+from django.urls import reverse
+
+from django.contrib.auth.models import Group
+
+from core import constants
+
+@pytest.mark.xfail
+@pytest.mark.parametrize('group,code', [
+    (None, 403),
+    (constants.Groups.VIP, 403),
+    (constants.Groups.DATA_STEWARD, 200),
+    (constants.Groups.AUDITOR, 403),
+    (constants.Groups.LEGAL, 403),
+])
+def test_permissions_on_create_view(django_user_model, client, group, code):
+    url = reverse('users_add')
+    user = django_user_model.objects.create(username='test.user')
+    user.set_password('pwd')
+    if group is not None:
+        g, _ = Group.objects.get_or_create(name=group.value)
+        user.groups.add(g)
+    user.save()
+    client.login(username=user.username, password='pwd')
+    response = client.post(url, data={})
+    assert response.status_code == code
+
+@pytest.mark.xfail
+@pytest.mark.parametrize('group,code', [
+    (None, 403),
+    (constants.Groups.VIP, 403),
+    (constants.Groups.DATA_STEWARD, 200),
+    (constants.Groups.AUDITOR, 403),
+    (constants.Groups.LEGAL, 403),
+])
+def test_permissions_on_edit_view(django_user_model, client, group, code):
+    user = django_user_model.objects.create(username='test.user')
+    user.set_password('pwd')
+    if group is not None:
+        g, _ = Group.objects.get_or_create(name=group.value)
+        user.groups.add(g)
+    user.save()
+    url = reverse('user_edit', args=[user.pk])
+    client.login(username=user.username, password='pwd')
+    response = client.post(url, data={})
+    assert response.status_code == code
