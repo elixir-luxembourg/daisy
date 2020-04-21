77ff1767ba8a3fbb4121dd7a14fdfc53f70a294d
Jacek Lebioda <jacek.lebioda@uni.lu>
2020-04-21 19:10:51 +0200

    feat: new dashboard

--- web/views/dashboard.py
+++ web/views/dashboard.py
@@ -4,10 +4,16 @@ from core.models import Dataset, Project
 
 
 def dashboard(request):
+    the_user = request.user
 
+    # Getting projects related to the user
+    projects_user_owns = Project.objects.filter(local_custodians=the_user)
+    # projects_user_is_in = Project.objects.filter()
+    # last_projects = Project.objects.filter().order_by('added')[:5]
+    last_projects = projects_user_owns.order_by('added')[:5]
+
+    # Getting datasets related to the user
     last_datasets = Dataset.objects.filter().order_by('added')[:5]
-    last_projects = Project.objects.filter().order_by('added')[:5]
-
 
     context = {
         "last_datasets": last_datasets,
