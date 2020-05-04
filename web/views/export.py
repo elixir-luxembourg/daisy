b8bc944425be00e97a4e9c934c21878d11f05fe3
Jacek Lebioda <jacek.lebioda@uni.lu>
2020-05-04 10:08:57 +0200

    fix: in case of errors, show message instead of throwing an exception

--- web/views/export.py
+++ web/views/export.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.contrib.admin.views.decorators import staff_member_required
+from django.http import HttpResponse
 
 from excel_response import ExcelResponse
 
@@ -11,22 +12,32 @@ log = DaisyLogger(__name__)
 
 
 @staff_member_required
-def generic_export(request, object_class, object_name):
-    query = request.GET.get('query', '')
-    order_by = request.GET.get('order_by', '')
-    objects = facet_view_utils.search_objects(
-        request,
-        filters=request.GET.getlist('filters'),
-        query=query,
-        object_model=object_class,
-        facets=settings.FACET_FIELDS[object_name],
-        order_by=order_by
-    )
+def generic_export(request, object_model_class, object_name):
+    def _do_export(request, object_model_class, object_name):
+        query = request.GET.get('query', '')
+        order_by = request.GET.get('order_by', '')
+        objects = facet_view_utils.search_objects(
+            request,
+            filters=request.GET.getlist('filters'),
+            query=query,
+            object_model=object_model_class,
+            facets=settings.FACET_FIELDS[object_name],
+            order_by=order_by
+        )
 
-    objects_ids = [x.__dict__['pk'] for x in objects]
-    objects = object_class.objects.filter(id__in=objects_ids)
-    values = [x.serialize_to_export() for x in objects]
-    return ExcelResponse(values)
+        objects_ids = [obj.__dict__['pk'] for obj in objects]
+        objects = object_model_class.objects.filter(id__in=objects_ids)
+        values = [obj.serialize_to_export() for obj in objects]
+
+        if len(values) == 0:
+            raise ValueError("There are no values to export - your selection was empty")
+        return ExcelResponse(values)
+    
+    try:
+        response = _do_export(request, object_model_class, object_name)
+        return response
+    except Exception as e:
+        return HttpResponse(f'There was a problem with export: \r\n{str(e)}')
 
 def cohorts_export(request):
     return generic_export(request, Cohort, 'cohort')
