51bbf41b0981f2ab68d9f948e0be2d738dad1912
Jacek Lebioda <jacek.lebioda@uni.lu>
2020-05-08 13:23:37 +0200

    feat: latest notifications

--- web/views/dashboard.py
+++ web/views/dashboard.py
@@ -8,17 +8,27 @@ def dashboard(request):
 
     # Getting projects related to the user
     projects_user_owns = Project.objects.filter(local_custodians=the_user)
-    # projects_user_is_in = Project.objects.filter()
-    # last_projects = Project.objects.filter().order_by('added')[:5]
-    last_projects = projects_user_owns.order_by('added')[:5]
+    projects_user_is_in = Project.objects.filter(company_personnel=the_user)
+    
+    # Take only 5 most recent
+    projects_user_owns_latest = projects_user_owns.order_by('added')[:5]
+    projects_user_is_in_latest = projects_user_is_in.order_by('added')[:5]
+
+    # Join together these projects
+    last_projects = projects_user_owns_latest.union(projects_user_is_in_latest)
 
     # Getting datasets related to the user
-    last_datasets = Dataset.objects.filter().order_by('added')[:5]
+    last_datasets = Dataset.objects.filter(local_custodians=the_user).order_by('added')[:5]
+
+    # Get the notifications
+    notifications = request.user.notifications.ordered()[:5]
 
     context = {
         "last_datasets": last_datasets,
-        "last_projects": last_projects
+        "last_projects": last_projects,
+        "notifications": notifications,
     }
+
     return render(
         request,
         'dashboard.html', context
