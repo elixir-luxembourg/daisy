744787602d086797dc0b94cb1f9a11410f52e472
Jacek Lebioda <jacek.lebioda@uni.lu>
2021-01-05 15:50:19 +0100

    chore: removed unused constant

--- core/management/commands/export_partners.py
+++ core/management/commands/export_partners.py
@@ -2,8 +2,6 @@ from core.management.commands._private import ExportBaseCommand
 
 from core.importer.partners_exporter import PartnersExporter
 
-JSON_SUFFIX = '.json'
-
 
 class Command(ExportBaseCommand):
     help = 'Export partner records to a designated file'
--- core/management/commands/export_projects.py
+++ core/management/commands/export_projects.py
@@ -1,8 +1,6 @@
 from core.management.commands._private import ExportBaseCommand
 from core.importer.projects_exporter import ProjectsExporter
 
-JSON_SUFFIX = '.json'
-
 
 class Command(ExportBaseCommand):
     help = 'export project records to a designated file'
