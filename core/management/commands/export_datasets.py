16d1d5e0ab9e5dd534cba1f3e731d641d2d71d02
Jacek Lebioda <jacek.lebioda@uni.lu>
2021-01-05 15:50:19 +0100

    chore: removed unused constant

--- core/management/commands/export_datasets.py
+++ core/management/commands/export_datasets.py
@@ -1,8 +1,6 @@
 from core.management.commands._private import ExportBaseCommand
 from core.importer.datasets_exporter import DatasetsExporter
 
-JSON_SUFFIX = '.json'
-
 
 class Command(ExportBaseCommand):
     help = 'export dataset records to a designated file'
--- core/management/commands/import_datasets.py
+++ core/management/commands/import_datasets.py
@@ -1,8 +1,6 @@
 from core.management.commands._private import ImportBaseCommand
 from core.importer.datasets_importer import DatasetsImporter
 
-JSON_SUFFIX = '.json'
-
 
 class Command(ImportBaseCommand):
     help = 'Import datasets from JSON file(s)'
