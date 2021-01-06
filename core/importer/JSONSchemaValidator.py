0fbb73ca15d66d07ce12bb90558e4ecf098b21ac
Jacek Lebioda <jacek.lebioda@uni.lu>
2021-01-06 17:10:19 +0100

    fix: corrections in schema validator

--- core/importer/JSONSchemaValidator.py
+++ core/importer/JSONSchemaValidator.py
@@ -6,7 +6,7 @@ import urllib.request
 from core.exceptions import JSONSchemaValidationError
 
 
-JSONSCHEMA_BASE_LOCAL_PATH = 'core/fixtures/'
+JSONSCHEMA_BASE_LOCAL_PATH = './core/fixtures/'
 JSONSCHEMA_BASE_REMOTE_URL = "https://git-r3lab.uni.lu/pinar.alper/metadata-tools/raw/master/metadata_tools/resources/"
 
 
@@ -35,13 +35,20 @@ class BaseJSONSchemaValidator:
     def _preload_schema(self):
         try:
             self._cached_schema = self._load_schema_from_disk()
-        except:
-            print("Error loading schema from disk for JSON validation...", file=sys.stderr) 
+            return 
+        except Exception as e:
+            import os
+            print("Error loading schema from disk for JSON validation...: " + str(e), file=sys.stderr) 
+            print("Working directory = " + os.getcwd(), file=sys.stderr) 
+            print("File path = " + self.base_path + self.schema_name, file=sys.stderr)
+            print("\nWill try to load the schema from URL...", file=sys.stderr)
 
         try:
             self._cached_schema = self._load_schema_from_url()
+            return
         except:
-            print("Error loading schema from URI for JSON validation...", file=sys.stderr)
+            print("Error loading schema from URI for JSON validation...: " + str(e), file=sys.stderr)
+            print("URL = " + self.base_url + self.schema_name, file=sys.stderr) 
 
         raise Exception('Cannot load schema for JSON validation')
 
