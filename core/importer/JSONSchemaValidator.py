09c68ba6dac5c5774389f7f98a72c6d92cf18c5e
Jacek Lebioda <jacek.lebioda@uni.lu>
2021-01-22 15:40:28 +0100

    chore: changed print to logger in json validation error handling

--- core/importer/JSONSchemaValidator.py
+++ core/importer/JSONSchemaValidator.py
@@ -4,7 +4,10 @@ import sys
 import urllib.request
 
 from core.exceptions import JSONSchemaValidationError
+from core.utils import DaisyLogger
 
+
+logger = DaisyLogger(__name__)
 
 JSONSCHEMA_BASE_LOCAL_PATH = './core/fixtures/'
 JSONSCHEMA_BASE_REMOTE_URL = "https://git-r3lab.uni.lu/pinar.alper/metadata-tools/raw/master/metadata_tools/resources/"
@@ -38,17 +41,17 @@ class BaseJSONSchemaValidator:
             return 
         except Exception as e:
             import os
-            print("Error loading schema from disk for JSON validation...: " + str(e), file=sys.stderr) 
-            print("Working directory = " + os.getcwd(), file=sys.stderr) 
-            print("File path = " + self.base_path + self.schema_name, file=sys.stderr)
-            print("\nWill try to load the schema from URL...", file=sys.stderr)
+            logger.warn("Error (1/2) loading schema from disk for JSON validation...: " + str(e))
+            logger.warn("Working directory = " + os.getcwd()) 
+            logger.warn("File path = " + self.base_path + self.schema_name)
+            logger.warn("Will try to load the schema from URL...")
 
         try:
             self._cached_schema = self._load_schema_from_url()
             return
         except:
-            print("Error loading schema from URI for JSON validation...: " + str(e), file=sys.stderr)
-            print("URL = " + self.base_url + self.schema_name, file=sys.stderr) 
+            logger.error("Error (2/2) loading schema from URI for JSON validation...: " + str(e))
+            logger.error("URL = " + self.base_url + self.schema_name) 
 
         raise Exception('Cannot load schema for JSON validation')
 
