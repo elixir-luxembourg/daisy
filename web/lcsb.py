e7a8925e1d268e6892e96e1a3b9e283318920b87
Jacek Lebioda <jacek.lebioda@uni.lu>
2021-06-10 15:11:43 +0200

    feat: log data coming from REMS

--- web/lcsb.py
+++ web/lcsb.py
@@ -1,13 +1,22 @@
-from typing import Dict
+import logging
 
 from typing import Dict
 
+from core.utils import DaisyLogger
+
+
+logger = DaisyLogger(__name__)
 
 def handle_rems_callback(request_post_data: Dict):
     user = request_post_data.POST.get('user')
     application = request_post_data.POST.get('application')
     resource = request_post_data.get('resource')
     email = request_post_data.get('email')
+    logger.debug('Received from REMS:')
+    logger.debug(user)
+    logger.debug(application)
+    logger.debug(resource)
+    logger.debug(email)
     raise NotImplemented()
     # TODO
     return True
