36b22dd664ff384885c8d645f04f28b62788aa37
Jacek Lebioda <jacek.lebioda@uni.lu>
2021-06-10 16:59:54 +0200

    fix: typo

--- web/lcsb.py
+++ web/lcsb.py
@@ -8,8 +8,8 @@ from core.utils import DaisyLogger
 logger = DaisyLogger(__name__)
 
 def handle_rems_callback(request_post_data: Dict):
-    user = request_post_data.POST.get('user')
-    application = request_post_data.POST.get('application')
+    user = request_post_data.get('user')
+    application = request_post_data.get('application')
     resource = request_post_data.get('resource')
     email = request_post_data.get('email')
     logger.debug('Received from REMS:')
