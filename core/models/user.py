e4974f4d311291741bf32b3fed38ff6180c1185e
Vilem Ded <vilem.ded@uni.lu>
2021-06-24 12:44:58 +0200

    Revert "add logbook entry on REMS access request"
    This reverts commit 9644d913a86354ee92607a538b4a06192cf1a7ad.

--- core/models/user.py
+++ core/models/user.py
@@ -9,7 +9,7 @@ from enumchoicefield.enum import ChoiceEnum
 from guardian.shortcuts import assign_perm, remove_perm
 
 from core import constants
-from core.models import Access, Dataset, Share, DataLogType
+from core.models import Access, Dataset
 from core.permissions import ProjectChecker, DatasetChecker, ContractChecker, AutoChecker
 from .utils import TextFieldWithInputWidget
 
@@ -228,20 +228,11 @@ class User(AbstractUser):
         """
         notes = f'Set automatically by REMS application #{application}'
         dataset = Dataset.objects.get(elu_accession=resource)
-        new_access = Access(
+        new_logbook_entry = Access(
             user=self,
             dataset=dataset,
             access_notes=notes,
             granted_on=datetime.now()
         )
-        new_access.save()
-
-        new_logbook_entry = Share(
-            dataset=dataset,
-            granted_on=datetime.now(),
-            share_notes='rems-application:'+application,
-            data_log_type=DataLogType.objects.get(name='Other')
-        )
         new_logbook_entry.save()
-
         return True
