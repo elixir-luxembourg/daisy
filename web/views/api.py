d30eb1d3046066560432547001a27daad7451fe1
Jacek Lebioda <jacek.lebioda@uni.lu>
2021-06-24 16:07:12 +0200

    chore: small refactoring of api.py

--- web/views/api.py
+++ web/views/api.py
@@ -29,7 +29,7 @@ from web.views.utils import get_client_ip
 logger = DaisyLogger(__name__)
 
 
-def error_response(message: str, more: Optional[Dict]={}, status: int=500) -> JsonResponse:
+def create_error_response(message: str, more: Optional[Dict]={}, status: int=500) -> JsonResponse:
     body = {
         'status': 'Error',
         'description': message
@@ -47,7 +47,7 @@ def protect_with_api_key(view):
         submitted_keys = [request.GET.get('API_KEY', '-'), request.POST.get('API_KEY', '-')]
         error_message = 'API_KEY missing in POST or GET parameters, or its value is invalid!'
         if 'API_KEY' not in request.GET and 'API_KEY' not in request.POST:
-            return error_response(error_message, status=403)
+            return create_error_response(error_message, status=403)
         elif hasattr(settings, 'GLOBAL_API_KEY') and getattr(settings, 'GLOBAL_API_KEY') in submitted_keys:
             return view(request, *args, **kwargs)
         # Check the key from GET
@@ -56,7 +56,7 @@ def protect_with_api_key(view):
         # Check the key from POST
         elif submitted_keys[1] not in ['-', ''] and User.objects.filter(api_key=submitted_keys[1]).count() > 0:
             return view(request, *args, **kwargs)
-        return error_response(error_message, status=403)
+        return create_error_response(error_message, status=403)
     return decorator
 
 """
@@ -136,7 +136,7 @@ def datasets(request):
 
         return HttpResponse(buffer.getvalue())
     except Exception as e:
-        return error_response(
+        return create_error_response(
             'Something went wrong during exporting the datasets',
             {'more': str(e)}
         )
@@ -158,7 +158,7 @@ def projects(request):
 
         return HttpResponse(buffer.getvalue())
     except Exception as e:
-        return error_response(
+        return create_error_response(
             'Something went wrong during exporting the projects',
             {'more': str(e)}
         )
@@ -169,7 +169,7 @@ def rems_endpoint(request):
     if not getattr(settings, 'REMS_INTEGRATION_ENABLED', False):
         message = f'REMS endpoint called, but it''s disabled.'
         logger.debug(message)
-        return error_response(message)
+        return create_error_response(message)
         
     ip = get_client_ip(request)
     logger.debug(f'REMS endpoint called from: {ip}...')
@@ -182,12 +182,12 @@ def rems_endpoint(request):
     if len(allowed_ips) == 0 and not skip_check_setting:
         message = f'REMS - the list of allowed IPs is empty, import failed!'
         logger.debug(message)
-        return error_response(message)
+        return create_error_response(message)
 
     if ip not in allowed_ips and not skip_check_setting:
         message = f'REMS - the IP is not in the list of allowed IPs, import failed!'
         logger.debug(message)
-        return error_response(message)
+        return create_error_response(message)
     
     try:
         status = "Success" if handle_rems_callback(request) else "Failure"
@@ -196,19 +196,19 @@ def rems_endpoint(request):
     except Exception as ex:
         message = f'REMS - something went wrong during the import!'
         logger.debug(message)
-        return error_response(message, {'more': str(ex)})
+        return create_error_response(message, {'more': str(ex)})
 
 
 @public
 @csrf_exempt
 @protect_with_api_key
-def permissions(request, user_id: str):
+def permissions(request, user_oidc_id: str) -> JsonResponse:
     try:
-        user = User.objects.get(id=user_id)
+        user = User.objects.get(oidc_id=user_oidc_id)
         permissions = user.get_access_permissions()
         return JsonResponse(permissions, status=200, safe=False)
     except Exception as e:
-        return error_response(
+        return create_error_response(
             'Something went wrong during exporting the permissions',
             {'more': str(e)}
         )
