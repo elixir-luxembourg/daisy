415761099178e4cca5b9310f8d1b2422755a41db
Vilem Ded <vilem.ded@uni.lu>
2021-07-13 17:27:17 +0200

    test: add client_user_admin

--- web/tests/conftest.py
+++ web/tests/conftest.py
@@ -9,3 +9,8 @@ def client_user_normal(client, user_normal):
 def client_user_vip(client, user_vip):
     client.login(username=user_vip.username, password=user_vip.password)
     return client
+
+@pytest.fixture
+def client_user_admin(client, user_admin):
+    client.login(username=user_admin.username, password="password") #TODO: the password is hardcoded since user_admin.password returns just hash
+    return client
