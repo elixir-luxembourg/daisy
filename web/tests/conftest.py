2043396b0b5fae414394869cf89563528055a805
Vilem Ded <vilem.ded@uni.lu>
2021-07-13 17:27:52 +0200

    test: add client_user_data_steward login

--- web/tests/conftest.py
+++ web/tests/conftest.py
@@ -11,6 +11,11 @@ def client_user_vip(client, user_vip):
     return client
 
 @pytest.fixture
+def client_user_data_steward(client, user_data_steward):
+    client.login(username=user_data_steward.username, password=user_data_steward.password)
+    return client
+
+@pytest.fixture
 def client_user_admin(client, user_admin):
     client.login(username=user_admin.username, password="password") #TODO: the password is hardcoded since user_admin.password returns just hash
     return client
