4ba5ff8721e9b6e051f7a8519af82d20d268b45e
Vilem Ded <vilem.ded@uni.lu>
2021-07-13 17:50:18 +0200

    test: test excel export

--- web/tests/test_excel_export.py
+++ web/tests/test_excel_export.py
@@ -1,24 +1,29 @@
 import pytest
 from test import factories
 
-
 from django.urls import reverse
 
-@pytest.mark.skip(reason="TBD")
-@pytest.mark.parametrize('url_name,factory,model', [
-    ('datasets_export', factories.DatasetFactory, 'Dataset'),
-    ('projects_export', factories.ProjectFactory, 'Project'),
-    ('contracts_export', factories.ContractFactory, 'Contract'),
-    ('partners_export', factories.PartnerFactory, 'Partner'),
-    ('cohorts_export', factories.CohortFactory, 'Cohort'),
+
+@pytest.mark.parametrize('url_name,factory', [
+    ('datasets_export', factories.DatasetFactory),
+    ('projects_export', factories.ProjectFactory),
+    ('contracts_export', factories.ContractFactory),
+    ('partners_export', factories.PartnerFactory),
+    ('cohorts_export', factories.CohortFactory),
 ])
-def test_excel_export(client_user_vip,  url_name, factory, model):
+def test_excel_export(client_user_admin, url_name, factory):
     """
     Test view for exporting records to Excel file.
     """
-    
-    # TODO: The database is probably empty at the moment
 
+    number_of_objects = 4
+    objects = factory.create_batch(number_of_objects)
     url = reverse(url_name)
-    response = client_user_vip.get(url)
+    response = client_user_admin.get(url)
+
     assert response.status_code == 200
+
+    #TODO: verify the records are correct - and number of records matches
+    #with io.BytesIO(response.content) as fh:
+    #    pass
+    #    df = pd.io.excel.read_excel(fh, sheetname=0)
