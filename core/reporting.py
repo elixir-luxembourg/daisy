ee1c58bb8f44ec4aa7eaa534ac5b4a757b7a93c2
Jacek Lebioda <jacek.lebioda@uni.lu>
2021-09-10 14:26:29 +0200

    feat: email reporting - gathering data

--- core/reporting.py
+++ core/reporting.py
@@ -1,11 +1,26 @@
-
+from functools import reduce
+from operator import concat
 from typing import List
 
 from django.template.loader import render_to_string
+from core.models import data_declaration
 
-from core.models.project import Project
 from core.models.dataset import Dataset
 from core.models.data_declaration import DataDeclaration
+from core.models.project import Project
+from core.models.user import User
+
+
+class Issue:
+    def __init__(self,
+            url: str,
+            code: str,
+            description: str,
+            object_title: str) -> None:
+        self.url = url
+        self.object_title = object_title
+        self.description = description
+        self.code = code
 
 
 class ReportParameters:
@@ -17,11 +32,32 @@ class ReportParameters:
                  projects: List[Project] = None,
                  datasets: List[Dataset] = None,
                  data_declarations: List[DataDeclaration] = None,
-                 issues: List = None) -> None:
+                 issues: List[Issue] = None) -> None:
         self.issues = issues
         self.datasets = datasets
         self.data_declarations = data_declarations
         self.projects = projects
+
+
+class ReportParametersCollector:
+    @classmethod
+    def generate_for_user(cls, user: User):
+        projects = Project.objects.filter(local_custodian__in=User)
+        # TODO: What exactly needs to be collected here:
+        # 1) datasets whose local_custodian is the User
+        # 2) datasets linked to the project
+        # 3) everything, 1 + 2 
+
+        datasets = Dataset.objects.filter(local_custodian__in=User)
+
+        data_declarations_list = [dataset.data_declarations for dataset in datasets]
+        data_declarations_with_repetitions = reduce(concat, data_declarations_list)
+        data_declarations = list(set(data_declarations_with_repetitions))
+
+        # TODO: Generate Issues
+        issues = None
+        
+        return ReportParameters(projects, datasets, data_declarations, issues)
 
 
 class ReportRenderer:
