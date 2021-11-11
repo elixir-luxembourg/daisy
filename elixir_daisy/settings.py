5bfc1d341efb0451a86591cf06f1ffee863fc5ae
Vilem Ded <vilem.ded@uni.lu>
2021-11-11 14:16:05 +0100

    feat: put DSW_ORIGIN in settings.py

--- elixir_daisy/settings.py
+++ elixir_daisy/settings.py
@@ -341,6 +341,9 @@ REMS_ALLOWED_IP_ADDRESSES = []  # use '*' to allow all, otherwise e.g. '127.0.0.
 # ID service
 IDSERVICE_FUNCTION = 'web.views.utils.generate_elu_accession'
 
+# Data Stewardship Wizard - pop up integration
+DSW_ORIGIN = 'localhost'
+
 # Import local settings to override those values based on the deployment environment
 try:
     from .settings_local import *
