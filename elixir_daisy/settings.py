72198669a82ef92caaa4ce83ff8affb90e39f8ef
Jacek Lebioda <jacek.lebioda@uni.lu>
2021-12-14 12:09:29 +0100

    chore: remove staticfiles panel from DDT

--- elixir_daisy/settings.py
+++ elixir_daisy/settings.py
@@ -346,3 +346,20 @@ try:
     from .settings_local import *
 except ImportError as e:
     pass
+
+if DEBUG:
+    # Removing staticfiles panel from Django Debug Toolbar
+    DEBUG_TOOLBAR_PANELS = [
+        'debug_toolbar.panels.versions.VersionsPanel',
+        'debug_toolbar.panels.timer.TimerPanel',
+        'debug_toolbar.panels.settings.SettingsPanel',
+        'debug_toolbar.panels.headers.HeadersPanel',
+        'debug_toolbar.panels.request.RequestPanel',
+        'debug_toolbar.panels.sql.SQLPanel',
+        'debug_toolbar.panels.templates.TemplatesPanel',
+        'debug_toolbar.panels.cache.CachePanel',
+        'debug_toolbar.panels.signals.SignalsPanel',
+        'debug_toolbar.panels.logging.LoggingPanel',
+        'debug_toolbar.panels.redirects.RedirectsPanel',
+        'debug_toolbar.panels.profiling.ProfilingPanel',
+    ]
