09987806238d49a58fd5c4d360724a75bbac0db2
francois.ancien <francois.ancien@uni.lu>
2022-09-12 11:32:46 +0200

    Moving object_list building to get_context_data in log_entry view

--- web/views/log_entry.py
+++ web/views/log_entry.py
@@ -5,7 +5,9 @@ from guardian.mixins import PermissionRequiredMixin
 from auditlog.models import LogEntry
 import datetime
 
+
 class LogEntryListView(PermissionRequiredMixin, ListView):
+    DATE_FORMAT = "%Y-%m-%d"
     model = LogEntry
     paginate_by = 5
     allow_empty = True
@@ -13,66 +15,45 @@ class LogEntryListView(PermissionRequiredMixin, ListView):
 
     template_name = 'history/log_entry_list.html'
 
-    def get_context_data(self, start_date=None, end_date=None, object_list=None, **kwargs):
-        if start_date is None:
-            start_date = datetime.date.today() - datetime.timedelta(days=90)
-            start_date = start_date.strftime("%Y-%m-%d")
-        if end_date is None:
-            end_date = datetime.date.today().strftime("%Y-%m-%d")
-
-        models_list_fk = LogEntry.objects.values("content_type").distinct().all()
-        models_list_names = [ct["model"] for ct in ContentType.objects.values("model").filter(pk__in=models_list_fk).all()]
-        context = super().get_context_data(**kwargs)
-        context["models_list"] = models_list_names
-        context["start_date"] = start_date
-        context["end_date"] = end_date
-
-        # FIXME
-        if "parent_entity_name" in kwargs.keys() and "parent_entity_id" in kwargs.keys():
-            context["parent_entity_name"] = kwargs.get("parent_entity_name")
-            context["parent_entity_id"] = kwargs.get("parent_entity_id")
-
-        if "user" in kwargs.keys():
-            context["user"] = kwargs.get("user")
-
-        return context
-
-    def get(self, request, *args, **kwargs):
-        if "entity_name" in request.GET.keys() and "entity_id" in request.GET.keys():
-            entity_class = get_object_or_404(ContentType, model=request.GET.get("entity_name")).model_class()
-            entity_object = get_object_or_404(entity_class, pk=request.GET.get("entity_id"))
+    def get_context_data(self, object_list=None, filters=None, **kwargs):
+        if "entity_name" in filters.keys() and "entity_id" in filters.keys():
+            entity_class = get_object_or_404(ContentType, model=filters.get("entity_name")).model_class()
+            entity_object = get_object_or_404(entity_class, pk=filters.get("entity_id"))
             self.object_list = [{"action": log.Action.choices[log.action], "log": log} for log in entity_object.history.all()]
-            context = self.get_context_data()
-            return self.render_to_response(context)
 
-        filters = {}
-        if "entity_name" in request.GET.keys():
-            filters.update({"content_type__model": request.GET.get("entity_name")})
-            if "parent_entity_name" in request.GET.keys() and "parent_entity_id" in request.GET.keys():
-                entity_class = get_object_or_404(ContentType, model=request.GET.get("entity_name")).model_class()
+        query_filters = {}
+        if "entity_name" in filters.keys():
+            query_filters.update({"content_type__model": filters.get("entity_name")})
+            if "parent_entity_name" in filters.keys() and "parent_entity_id" in filters.keys():
+                entity_class = get_object_or_404(ContentType, model=filters.get("entity_name")).model_class()
                 entity_ids_list = [
-                    o.pk for o in entity_class.objects.filter(**{request.GET.get("parent_entity_name"): request.GET.get("parent_entity_id")})
+                    o.pk for o in entity_class.objects.filter(**{filters.get("parent_entity_name"): filters.get("parent_entity_id")})
                 ]
                 filters.update({"object_id__in": entity_ids_list})
 
-        if "start_date" in request.GET.keys():
-            date = datetime.datetime.strptime(request.GET.get("start_date"), "%Y-%m-%d")
-            filters.update({"timestamp__gte": date})
+        if "user" in filters.keys():
+            filters.update({"actor__exact": filters.get("user")})
 
-        if "end_date" in request.GET.keys():
-            date = datetime.datetime.strptime(request.GET.get("end_date"), "%Y-%m-%d") + datetime.timedelta(days=1)
-            filters.update({"timestamp__lte": date})
+        start_date = datetime.datetime.strptime(filters.get("start_date"), self.DATE_FORMAT) \
+            if "start_date" in filters.keys() \
+            else datetime.date.today() - datetime.timedelta(days=90)
+        query_filters.update({"timestamp__gte": start_date})
 
-        if "user" in request.GET.keys():
-            filters.update({"actor__exact": request.GET.get("user")})
+        end_date = datetime.datetime.strptime(filters.get("end_date"), self.DATE_FORMAT) \
+            if "end_date" in filters.keys() \
+            else datetime.date.today()
+        query_filters.update({"timestamp__lte": end_date})
 
-        self.object_list = [{"action": log.Action.choices[log.action], "log": log} for log in LogEntry.objects.filter(**filters)]
-        context = self.get_context_data(
-            start_date=request.GET.get("start_date", None),
-            end_date=request.GET.get("end_date", None),
-            **filters,
-        )
+        self.object_list = [{"action": log.Action.choices[log.action], "log": log} for log in LogEntry.objects.filter(**query_filters)]
+        context = super().get_context_data(**kwargs)
 
+        models_list_fk = LogEntry.objects.values("content_type").distinct().all()
+        models_list_names = [ct["model"] for ct in ContentType.objects.values("model").filter(pk__in=models_list_fk).all()]
+        context["models_list"] = models_list_names
+        context["start_date"] = start_date.strftime(self.DATE_FORMAT)
+        context["end_date"] = end_date.strftime(self.DATE_FORMAT)
+        return context
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(filters=request.GET)
         return self.render_to_response(context)
-
-
