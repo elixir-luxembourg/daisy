0acdd123204df5773db8525d460aac0c40810e32
Hesam KORKI <hesam.korki@uni.lu>
2023-10-19 16:44:20 +0200

    refine celery config and create the task definition

--- elixir_daisy/celery_app.py
+++ elixir_daisy/celery_app.py
@@ -2,9 +2,15 @@ import os
 
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "elixir_daisy.settings")
-
 app = Celery("daisy")
+
+# Using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
+# The uppercase name-space means that all Celery configuration options must
+#  be specified in uppercase instead of lowercase, and start with CELERY_, 
+#  so for example the task_always_eager setting becomes CELERY_TASK_ALWAYS_EAGER,
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
