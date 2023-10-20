1df94c5ea2bd0fda0845ef6d5c6e0624ebc01c9a
francois.ancien <francois.ancien@uni.lu>
2023-10-20 14:20:57 +0200

    Correcting tests

--- notification/tests/test_models.py
+++ notification/tests/test_models.py
@@ -7,7 +7,7 @@ from test.factories import DatasetFactory, DatasetNotificationFactory
     "factory, object_factory", [(DatasetNotificationFactory, DatasetFactory)]
 )
 def test_notification_urls(user_normal, factory, object_factory):
-    notification = factory(actor=user_normal, content_object=object_factory())
+    notification = factory(recipient=user_normal, content_object=object_factory())
     assert (
         notification.get_absolute_url()
         == notification.content_object.get_absolute_url()
