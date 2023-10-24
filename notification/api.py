10bc0dbdba4e1639fb68d70ea5b6e06f0599f5b0
francois.ancien <francois.ancien@uni.lu>
2023-10-24 09:26:49 +0200

    Creating api endpoints for React

--- notification/api.py
+++ notification/api.py
@@ -2,12 +2,20 @@ import json
 
 from django.views.decorators.http import require_http_methods
 from django.core.exceptions import PermissionDenied
+from django.db.models import QuerySet
 
 from notification.models import Notification
 
 
+def jsonify(data):
+    if isinstance(data, QuerySet):
+        data = list(data)
+
+    return json.dumps({"data": data})
+
+
 @require_http_methods(["PATCH"])
-def dismiss_notification(request, pk):
+def api_dismiss_notification(request, pk):
     notification = Notification.objects.get(pk)
     if request.user != notification.recipient:
         raise PermissionDenied(
@@ -17,6 +25,30 @@ def dismiss_notification(request, pk):
         notification.dismissed = True
         notification.save()
     except Exception:
-        return json.dumps({"success": False})
+        return jsonify(False)
 
-    return json.dumps({"success": True})
+    return jsonify(True)
+
+
+@require_http_methods(["GET"])
+def api_get_notifications(request):
+    notifications_list = Notification.objects.filter(
+        recipient__pk=request.user.pk
+    ).all()
+
+    return jsonify(notifications_list)
+
+
+@require_http_methods(["GET", "POST"])
+def api_get_notifications_admin(request):
+    if not request.user.is_staff:
+        raise PermissionDenied("Only admin users can access this api endpoint")
+
+    if request.method == "POST" and "pk" in request.POST:
+        notifications_list = Notification.objects.filter(
+            recipient__pk=request.POST["pk"]
+        ).all()
+    else:
+        notifications_list = Notification.objects.all()
+
+    return jsonify(notifications_list)
