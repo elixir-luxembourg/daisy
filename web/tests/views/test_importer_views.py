8e58d52ac178faaeb7bdad6a17dc9ce0b153179f
Moustapha Cheikh <moustapha.cheikh@uni.lu>
2023-10-26 11:48:46 +0200

    Refactor and improve importer test

--- web/tests/views/test_importer_views.py
+++ web/tests/views/test_importer_views.py
@@ -1,86 +1,76 @@
 import os
-from typing import Optional
+
+from django.contrib.auth.models import Group
 from django.core.files.uploadedfile import SimpleUploadedFile
 import pytest
-from django.contrib.auth.models import Group
 from django.shortcuts import reverse
-from django.test.client import Client
+
 from core.constants import Groups
-from test.factories import DataStewardGroup, UserFactory
-from core.models.user import User
 
 
-def login_test_user(
-    test_client: Client, user: User, password: Optional[str] = "test-user"
+def upload_test_file(client, model_type, user=None):
+    """
+    Helper function to upload a test file using the provided client and return the response.
+    Optionally, it logs in the given user before uploading.
+    """
+    if user:
+        client.logout()
+        client.login(username=user.username, password=user.password)
+
+    url = reverse("import_data", args=[model_type])
+
+    try:
+        with open(
+            f"{os.getcwd()}/data/demo/{model_type}.json", "rb"
+        ) as test_file_content:
+            uploaded_file = SimpleUploadedFile(
+                f"test_file_{model_type}.json",
+                test_file_content.read(),
+                content_type="application/json",
+            )
+            response = client.post(
+                url, {"file": uploaded_file}, HTTP_X_REQUESTED_WITH="XMLHttpRequest"
+            )
+        return response
+    except FileNotFoundError:
+        pytest.fail(f"Test data file for {model_type} not found!")
+
+
+@pytest.mark.parametrize(
+    "model_type",
+    [
+        "projects",
+        "datasets",
+        "partners",
+    ],
+)
+@pytest.mark.django_db
+def test_importer_requests(
+    user_normal,
+    user_vip,
+    client_user_data_steward,
+    model_type,
 ):
-    assert test_client.login(username=user.username, password=password), "Login failed"
+    # Test POST request for data steward
+    response = upload_test_file(client_user_data_steward, model_type)
+    assert response.status_code == 200
 
-
-@pytest.mark.parametrize(
-    "group, model_type",
-    [
-        (DataStewardGroup, "projects"),
-        (DataStewardGroup, "datasets"),
-        (DataStewardGroup, "partners"),
-    ],
-)
-@pytest.mark.django_db
-def test_importer_get_request(group, model_type):
-    """
-    Test the GET request to the import_data endpoint for different model types.
-
-    The test checks if users belonging to the DataStewardGroup can successfully
-    access the importer's form via AJAX requests for different model types
-    (projects, datasets, and partners).
-
-    Args:
-        group (Group): The user group, expected to be DataStewardGroup.
-        model_type (str): The model type which can be 'projects', 'datasets', or 'partners'.
-    """
-    user = UserFactory(groups=[group()])
-    client = Client()
-    login_test_user(client, user)
+    # Test GET request for data steward
     url = reverse("import_data", args=[model_type])
-    response = client.get(url, HTTP_X_REQUESTED_WITH="XMLHttpRequest")
-    if user.is_part_of(Group.objects.get(name=Groups.DATA_STEWARD.value)):
-        assert response.status_code == 200
+    response = client_user_data_steward.get(url, HTTP_X_REQUESTED_WITH="XMLHttpRequest")
+    assert response.status_code == 200
     assert "importer/import_form.html" in [t.name for t in response.templates]
-    client.logout()
 
+    for user in [user_normal, user_vip]:
+        # Test POST request for other users
+        response = upload_test_file(client_user_data_steward, model_type, user)
+        assert response.status_code == 403
 
-@pytest.mark.parametrize(
-    "group, model_type",
-    [
-        (DataStewardGroup, "projects"),
-        (DataStewardGroup, "datasets"),
-        (DataStewardGroup, "partners"),
-    ],
-)
-@pytest.mark.django_db
-def test_importer_post_request(group, model_type):
-    """
-    Test the POST request to the import_data endpoint for different model types.
-
-    The test verifies if users belonging to the DataStewardGroup can successfully
-    post data (in this case, JSON files) for different model types (projects, datasets,
-    and partners) and get a successful 200 response via AJAX requests.
-
-    Args:
-        group (Group): The user group, expected to be DataStewardGroup.
-        model_type (str): The model type which can be 'projects', 'datasets', or 'partners'.
-    """
-    user = UserFactory(groups=[group()])
-    client = Client()
-    login_test_user(client, user)
-    url = reverse("import_data", args=[model_type])
-    with open(f"{os.getcwd()}/data/demo/{model_type}.json", "rb") as test_file_content:
-        uploaded_file = SimpleUploadedFile(
-            f"test_file_{model_type}.json",
-            test_file_content.read(),
-            content_type="application/json",
+        # Test GET request for other users
+        client_user_data_steward.logout()
+        client_user_data_steward.login(username=user.username, password=user.password)
+        url = reverse("import_data", args=[model_type])
+        response = client_user_data_steward.get(
+            url, HTTP_X_REQUESTED_WITH="XMLHttpRequest"
         )
-        response = client.post(
-            url, {"file": uploaded_file}, HTTP_X_REQUESTED_WITH="XMLHttpRequest"
-        )
-        assert response.status_code == 200
-    client.logout()
+        assert response.status_code == 403
