b90f570de630fd71d2d08634f92d2280f1c9a09e
Valentin Grouès <valentin.groues@uni.lu>
2023-11-13 13:02:24 +0100

    add missing kc settings from template

--- elixir_daisy/settings_local.template.py
+++ elixir_daisy/settings_local.template.py
@@ -75,8 +75,10 @@ REMS_VERIFY_SSL = True
 IDSERVICE_ENDPOINT = "https://10.240.16.199:8080/v1/api/id"
 
 # Keycloak integration, uncomment and fill the values below
+# KEYCLOAK_INTEGRATION = True
 # KEYCLOAK_URL = 'https://root-address-to-your-keycloak.com:443'
-# KEYCLOAK_REALM = 'master'
+# KEYCLOAK_REALM_LOGIN = 'master'
+# KEYCLOAK_REALM_ADMIN = 'master'
 # KEYCLOAK_USER = 'your service user for daisy'
 # KEYCLOAK_PASS = 'the password for the service user'
 
