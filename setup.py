e2dc93f29a33155502c3fc5aa5bb051ab2f6aa36
Moustapha Cheikh <moustapha.cheikh@uni.lu>
2024-02-06 09:43:31 +0100

    Standardize quote usage in setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ requirements = [
     "Django==3.2.23",
     "django-auth-ldap==4.1.0",
     "celery==5.2.3",
-    'setuptools<59.7.0,>=59.1.1',
+    "setuptools<59.7.0,>=59.1.1",
     "celery-haystack-ng",
     "django-celery-beat==2.3.0",
     "django-celery-results==2.4.0",
