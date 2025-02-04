560c690c895dda2dcefa735822ba35e448a63ad9
Vilem Ded <vilemded@gmail.com>
2025-02-04 14:52:48 +0100

    formatting

--- core/migrations/0039_ip_restriction_class.py
+++ core/migrations/0039_ip_restriction_class.py
@@ -1,5 +1,6 @@
 from django.db import migrations
 
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -7,17 +8,17 @@ class Migration(migrations.Migration):
     ]
 
     def add_ip_restriction_class(apps, schema_editor):
-        restriction_class_model=apps.get_model("core", "RestrictionClass")
+        restriction_class_model = apps.get_model("core", "RestrictionClass")
         restriction_class_model.objects.get_or_create(
             code="IP",
             name="IP claim terms",
-            description="Intellectual property rights claims."
+            description="Intellectual property rights claims.",
         )
+
         restriction_class_model.objects.get_or_create(
             code="Other",
             name="Other use restrictions",
-            description="Other use restrictions."
+            description="Other use restrictions.",
         )
-
 
     operations = [migrations.RunPython(add_ip_restriction_class)]
