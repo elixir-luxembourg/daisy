5a2a9722a492df5d5980a18d091689ad5a6457bc
Aleksandr KOZAREV <aleksandr.kozarev@unilxw1vp42mx.uni.lux>
2025-07-16 10:56:50 +0200

    rm unused datasets property

--- core/models/dac.py
+++ core/models/dac.py
@@ -64,13 +64,6 @@ class DAC(CoreModel):
     )
 
     @property
-    def datasets(self):
-        """
-        Returns the datasets associated with the DAC.
-        """
-        return self.datasets.all()
-
-    @property
     def project(self):
         """
         Returns the projects associated with the DAC's contract.
