fd967349fc15a8502d96870e70dfa5d71a4ac042
Amira KARCHOUD <amira.karchoud@unik7jtwkggj4.uni.lux>
2026-02-19 11:57:59 +0100

    fix index

--- core/search_indexes.py
+++ core/search_indexes.py
@@ -135,8 +135,6 @@ class DatasetIndex(CelerySearchIndex, indexes.Indexable):
 
     unique_id = indexes.CharField(indexed=True, stored=True, faceted=True)
 
-    elu_accession = indexes.CharField(indexed=True, stored=True, faceted=True)
-
     def prepare_title(self, obj):
         return obj.title
 
@@ -155,8 +153,6 @@ class DatasetIndex(CelerySearchIndex, indexes.Indexable):
     def prepare_local_custodians(self, obj):
         return [u.full_name for u in obj.local_custodians.all()]
 
-    def prepare_elu_accession(self, obj):
-        return obj.elu_accession
 
     def get_updated_field(self):
         return "updated"
@@ -324,7 +320,6 @@ class ProjectIndex(CelerySearchIndex, indexes.Indexable):
     study_terms = indexes.MultiValueField(indexed=True, stored=True, faceted=True)
     title = indexes.CharField(indexed=True, stored=True, faceted=True)
     local_custodians = indexes.MultiValueField(indexed=True, stored=True, faceted=True)
-    elu_accession = indexes.CharField(indexed=True, stored=True, faceted=True)
 
     def prepare_contacts(self, obj):
         return [str(o) for o in obj.contacts.all()]
@@ -400,9 +395,6 @@ class ProjectIndex(CelerySearchIndex, indexes.Indexable):
 
     def prepare_title(self, obj):
         return obj.title
-
-    def prepare_elu_accession(self, obj):
-        return obj.elu_accession
 
 
 class DACIndex(CelerySearchIndex, indexes.Indexable):
